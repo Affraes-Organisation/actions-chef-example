4ce38abab71e29930c679bbd96d195c1007e01d7
Daniel Figucio <affrae@github.com>
2020-03-04 11:21:37 +1100

    fix quotes in default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
-File `test.txt` do
- Content ‘This is a test file”
+File 'test.txt' do
+ Content 'This is a test file'
  action :create
 end
