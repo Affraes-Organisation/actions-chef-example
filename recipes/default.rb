681f48dd6ee20892592b56e15591d40b6c7ebdd2
Daniel Figucio <affrae@users.noreply.github.com>
2020-03-04 23:01:37 -0800

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,11 +4,11 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
-file '/tmp/GoodbyeWorld.txt' do
- content 'Goodbye World!!'
+file '/tmp/HelloWorld.txt' do
+ content 'Hello World!'
 
  verify do |path|
-  open(path).read.include? 'Goodbye World!!'
+  open(path).read.include? 'Hello World!'
  end
 
  action :create
